aee1cfd1f9ff72cda48104aa757f198503e53a7f
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-30 16:10:09 -0400

    added questions to an array to use in prompt

--- index.js
+++ index.js
@@ -3,7 +3,44 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "Enter a brief description of the project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Explain how users can install your project:",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Explain how users can use your project:",
+        name: "usage"
+    },
+    {
+        type: "list",
+        message: "Which license do you want for your project:",
+        choices: ["MIT", "Apache", "ISC"],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Explain how users can contribute to your project:",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Explain the tests used when building this project:",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "Explain how users ask you questions about the project:",
+        name: "questions"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
