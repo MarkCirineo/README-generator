551e2188e47f60ee54b971ea0cd642481e76b30c
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-30 17:37:07 -0400

    added conditional for licenses, updated license section and updated questions section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
     if (license === "MIT") {
         return "![MIT license badge](https://img.shields.io/badge/license-MIT-brightgreen)"
@@ -10,17 +8,67 @@ function renderLicenseBadge(license) {
     }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseSection(license, year, fullName) {
+    if (license === "MIT") {
+        return `MIT License
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+Copyright (c) ${year} ${fullName}
 
-// TODO: Create a function to generate markdown for README
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`
+    } else if (license === "Apache") {
+        return `Copyright ${year} ${fullName}
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`
+    } else if (license === "ISC") {
+        return `ISC License
+
+Copyright (c) ${year} ${fullName}
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.`
+    }  
+}
+
 function generateMarkdown(data) {
     return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
 ## Description
 ${data.description}
 ## Table of Contents
@@ -35,15 +83,13 @@ ${data.installation}
 ## Usage
 ${data.usage}
 ## License
-This project uses the ${data.license} license.
-
-${renderLicenseBadge(data.license)}
+${renderLicenseSection(data.license)}
 ## How to Contribute
 ${data.contributing}
 ## Tests
 ${data.tests}
 ## Questions
-${data.questions}
+If you have any question you can view my GitHub Profile [here](https://www.github.com/${data.github}), or you can contact me here: ${data.email}
 `;
 }
 
