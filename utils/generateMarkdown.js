7c772a9f3dccaf391b45fb20ae9b8473479124f2
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-30 16:42:21 -0400

    created generateMarkdown function to generate the README using data from user inputs

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,28 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+    return `# ${data.title}
+## Description
+${data.description}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [How To Contribute](#how-to-contibute)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${data.license}
+## How to Contribute
+${data.contributing}
+## Tests
+${data.tests}
+## Questions
+${data.questions}
 `;
 }
 
